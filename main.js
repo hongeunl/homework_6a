b331cd34f6b3d53bdd250166456f6f70d25b872b
hongeunl <hongeunl@andrew.cmu.edu>
2021-10-25 00:07:03 -0400

    added comments

--- main.js
+++ main.js
@@ -1,89 +1,4 @@
-/* /* function showItems() {
-  let list = document.getElementById("listOfItems");
-  let quantity = document.getElementById("quantity");
-  var eachQuantity = quantity.options[quantity.selectedIndex].text;
-  let newQuant = document.createElement("li");
-  newQuant.appendChild(document.createTextNode(eachQuantity));
-  list.appendChild(newQuant);
-  console.log(list);
-} */
-
-function addToGlazeArray() {
-  var eachGlaze = [];
-  let glaze = document.getElementsByClassName("choose-glaze");
-  let chosenGlaze = glaze.options[glaze.selectedIndex].text;
-  eachGlaze.push(chosenGlaze);
-  console.log()
-}
-
-/* 
-
-function addToGlazeArray() {
-  var listOfGlaze = document.getElementById("listOfGlaze");
-  let chosenGlaze = document.querySelector('input[name="glaze"]:checked').value;
-  let newAdded = document.createElement("li");
-  newAdded.appendChild(chosenGlaze);
-  list.appendChild(listOfGlaze);
-  console.log(listOfGlaze);
-
-} */
-
-function addToQuantArray(eachGlaze) {
-  var eachQuant = [];
-  let quantity = document.getElementById("quantity");
-  let chosenQuantity = quantity.options[quantity.selectedIndex].text;
-  eachQuant.push(chosenQuantity);
-  if (eachQuant > 0) {
-    addEachLine(eachGlaze, eachQuant, eachQuant.length-1);
-    var lineAdded = addEachLine(eachGlaze, eachQuant, eachQuant.length-1);
-    showAllItems(lineAdded);
-  }
-}
-
-
-function addEachLine(eachGlaze, eachQuant, num) {
-  var list = document.getElementById("listOfItems");
-  let newAdded = document.createElement("li");
-  let combined = "ORIGINAL " + eachGlaze[num] + " x " + eachQuant[num];
-  newAdded.appendChild(combined);
-  list.appendChild(newAdded);
-  return list;
-}
-
-function showAllItems(list) {
-  console.log(list);
-}
-
-function Roll(name, glaze, price) {
-  this.name = name;
-  this.glaze = glaze;
-  this.price = price;
-}
-
-function addToCart(flavor) {
-  thisPrice = document.getElementById("totalPrice").value;
-  thisGlaze = document.getElementById("glaze").innerHTML;
-  thisFlavor = flavor;
-
-  newItem = new foodItem(flavor, thisGlaze, thisPrice);
-  cartArray.push(newItem);
-  console.log(cartArray);
-
-  updateCartNum();
-}
-
-var cartArray = [];
-
-function updateCartNum() {
-  var numItemsCart = cartArray.length;
-
-  if (numItemsCart > 0) {
-    var cartNum = document.getElementById("cart");
-    cartNum.innerHTML = "Cart " + numItemsCart;
-  }
-}
-
-
+//Change to different images when glaze is selected//
 function changeImage(value) {
   if (value == "none") {
     document.getElementById("roll-image").src = "cinnamonroll_large.png"
@@ -100,7 +15,7 @@ function changeImage(value) {
   changeTotalPrice(document.getElementsById("quantity").value);
 }
 
-
+//Update total price when glaze and quantity are selected//
 function changeTotalPrice(quantity) {
   var chosenGlaze = document.querySelector('input[name="glaze"]:checked').value;
   if (chosenGlaze == "none") {
@@ -112,3 +27,37 @@ function changeTotalPrice(quantity) {
     document.getElementById("rollprice").innerHTML = "$" + totalPrice.toFixed(2);
   }
 }
+
+var cartArray = [];
+
+//A new constructor//
+function Roll(name, glaze, price) {
+  this.name = name;
+  this.glaze = glaze;
+  this.price = price;
+}
+
+//Create an array of rolls that are put into the cart//
+function addToCart(flavor) {
+  thisPrice = document.getElementById("rollprice").value;
+  thisGlaze = document.querySelector('input[name="glaze"]:checked').value;
+  thisFlavor = flavor;
+
+  newItem = new Roll(flavor, thisGlaze, thisPrice);
+  cartArray.push(newItem);
+  console.log(cartArray);
+  updateCartNum();
+}
+
+//Update the number of items in the cart//
+function updateCartNum() {
+  var numItemsCart = cartArray.length;
+  if (numItemsCart > 0) {
+    var cartNum = document.getElementById("cart");
+    cartNum.innerHTML = "Cart " + numItemsCart;
+  }
+}
+
+
+
+
